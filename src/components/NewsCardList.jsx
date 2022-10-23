d8de61a8b5f15edaeb8281c7a15e84eb01d04641
MihaelaGrn <arond_com@yahoo.com>
2022-10-23 22:47:43 +0300

    update 37 v2

--- src/components/NewsCardList.jsx
+++ src/components/NewsCardList.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import NewsCard from "./NewsCard";
 
-function NewsCardList() {
-  return <div>NewsCardList</div>;
+function NewsCardList(props) {
+  const { newsList } = props;
+  return (
+    <Container>
+      <Row>
+        {newsList.map((news) => {
+          return (
+            <Col md={4} xs={12} lg={4}>
+              <NewsCard
+                imgSrc={news.imgSrc}
+                title={news.title}
+                description={news.description}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
 
 export default NewsCardList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import { useFetch } from "../utils/hooks/useFetch";
 import { getNewsCategoryEndpoint } from "../api/endpoints";
 import Layout from "../components/Layout";
-import NewsCard from "../components/NewsCard";
+import NewsCardList from "../components/NewsCardList";
 
 function Home() {
   const footballUrl = getNewsCategoryEndpoint("football", 1, 6);
@@ -14,21 +14,37 @@ function Home() {
   const techData = useFetch(techUrl);
   console.log(techData);
 
+  const newsList = [
+    {
+      id: "test1",
+      imgSrc:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiHFAAeHEYde_aXLL9qZT58-e2UmYlxQCEWg&usqp=CAU",
+      title: "Meciul de aseara",
+      description: "bla bla",
+    },
+    {
+      id: "test2",
+      imgSrc:
+        "https://e0.365dm.com/22/07/768x432/skysports-efl-ball_5848969.jpg?20220729160849",
+      title: "Meciul de alaltaieri",
+      description: "haha",
+    },
+  ];
+
   return (
     <Layout>
-      <Container>
-        <h1>Home</h1>
-        <NewsCard
-          imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiHFAAeHEYde_aXLL9qZT58-e2UmYlxQCEWg&usqp=CAU"
-          title="Meciul de aseara"
-          description="bla bla"
-        />
-        <NewsCard
-          imgSrc="https://e0.365dm.com/22/07/768x432/skysports-efl-ball_5848969.jpg?20220729160849"
-          title="Meciul de sambata"
-          description="lalala"
-        />
-      </Container>
+      <section>
+        <Container>
+          <h1>Tech</h1>
+          <NewsCardList newsList={newsList} />
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <h1>Football</h1>
+          <NewsCardList newsList={newsList} />
+        </Container>
+      </section>
     </Layout>
   );
 }
